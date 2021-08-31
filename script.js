738444b15a47cc8f1e2492e95ac4a2d1d6230027
Pheonix-Nate <89256921+Pheonix-Nate@users.noreply.github.com>
2021-08-31 09:30:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ let notify = document.getElementById('notify');
 notify.addEventListener('click', ()=>{
     alert('No notification for you here.')
 })
+let answers = ['#'];
 
 let mail = document.getElementById('mail');
 mail.addEventListener('click', (e)=>{
     e.preventDefault();
     prompt('Unknown: Hello!')
-    if (prompt == "#"){
-    alert('Was I seriously left on read?')
-}
 })
+//     if (prompt !== answers){
+//     alert('I did not think you would have answered. Sorry, bye!')
+// }
+// else{alert('I was left on read...')}
+
